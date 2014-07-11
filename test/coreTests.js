3e0cb258872088e99fe79284990881f54af61b85
Christopher Gillis <christopher.l.gillis@gmail.com>
2014-07-11 16:09:27 -0400

    Updated Assertable api - Removed internalIs - Replaced with isTrue and isFalse which take functions that must return   true or false in order to pass the assertion

--- test/coreTests.js
+++ test/coreTests.js
@@ -2,22 +2,26 @@ var expect = require('../totes').expect;
 
 var AssertionError = require('assertion-error');
 
-describe('internalIs', function(){
+describe('isTrue', function(){
 	it('should throw when assertion fails', function(done){
 		try{
-			expect('test string').internalIs(identity, 'not test string');
+			expect('test string').isTrue(assertion);
 			throw new AssertionError('Assertion did not throw')
 		}
 		catch(e){
 			done();
 		}
+
+		function assertion(val){
+			return val === 'not test string';	
+		}
 	});
 
 	it('should not throw when assertion succeeds', function(){
-		expect('test string').internalIs(identity, 'test string');
+		expect('test string').isTrue(assertion);
+
+		function assertion(val){
+			return val === 'test string';
+		}
 	});
 });
-
-function identity(i){
-	return i;
-}
