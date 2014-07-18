29d863534939c1f46cf7661e744062290023d03e
Christopher Gillis <christopher.l.gillis@gmail.com>
2014-07-18 16:06:19 -0400

    Test on initial 'gulp test' run

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('test', function(){
+	gulp.src('').pipe(shell('npm test'));
+
 	return gulp.watch(['lib/**/*.js', 'test/**/*.js'], function(){
 		gulp.src('')
 		.pipe(shell('npm test'));
